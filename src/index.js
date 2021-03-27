ccd68854bee5fd242f4922e98b74edd7f529497f
Marie Serykh <marie.serykh@gmail.com>
2021-03-27 15:24:41 +0100

    feat: implement functions 3 functions for seaching minimal, maximal and average value of a given array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === 'undefined') {
+    return 0;
+  }
+
+  let minElement = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (minElement > array[i]) {
+      minElement = array[i];
+    }
+  }
+  return minElement ?? 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === 'undefined') {
+    return 0;
+  }
+
+  let maxElement = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (maxElement < array[i]) {
+      maxElement = array[i];
+    }
+  }
+  return maxElement ?? 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === 'undefined') {
+    return 0;
+  } else if (!array.length) {
+    return 0;
+  }
+
+  let sum = 0;
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    sum += array[i];
+  }
+  const average = sum / length;
+  return average;
 }
